04902e6c77d59d08b88fa7d5333ae42c6a86973b
Helena Onezia Lewi <2381003@unai.edu>
2024-12-01 13:52:48 +0700

    interface service Event n Peserta

--- Main.java
+++ Main.java
@@ -1,7 +1,13 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void Main(String[] args) {
         System.out.println("Hello Helen, aman githbub collab kita? wkwk");
         System.out.println("Wokee bisa gess");
         System.out.println("karla bisa");
+    }
+
+    public static void clearScreen() {
+        for (int i = 0; i < 50; i++) {
+            System.out.println();
+        }
     }
 }
---
+++ service/EventnPesertaService.java
@@ -0,0 +1,12 @@
+package service;
+
+import entities.Event1;
+
+public interface EventnPesertaService {
+    EventList[] getEventList();
+    void addEvent(String nameEvent, String dateEvent, String eventLocation);
+    void tambahPeserta(String nama, String nim, Event1 eventpilih);
+    Boolean removeEvent(Integer number);
+    Boolean hapusPeserta(Integer number);
+    Boolean updateEvent(Integer number, String nameEvent, String dateEvent, String eventLocation);
+}
