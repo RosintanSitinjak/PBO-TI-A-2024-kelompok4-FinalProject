c2f93d2bd8682b39f0bb1493378fb2225a973e34
karlashanessatauran <2381044@unai.edu>
2024-12-01 09:20:31 +0700

    test

--- Main.java
+++ Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello Helen, aman githbub collab kita? wkwk");
         System.out.println("Wokee bisa gess");
+        System.out.println("karla bisa");
     }
 }
