5dfa5b26cc0835624f70bcf4942c36c80048c5a4
Rosintan Sitinjak <2381014@unai.edu>
2024-12-12 20:58:52 +0700

    menghapus bagian peserta

--- Main.java
+++ Main.java
@@ -1,13 +1,18 @@
-public class Main {
-    public static void Main(String[] args) {
-        System.out.println("Hello Helen, aman githbub collab kita? wkwk");
-        System.out.println("Wokee bisa gess");
-        System.out.println("karla bisa");
-    }
+package com.eventmanagement;
 
-    public static void clearScreen() {
-        for (int i = 0; i < 50; i++) {
-            System.out.println();
-        }
+import com.eventmanagement.repositories.EventRepository;
+import com.eventmanagement.repositories.ParticipantRepository;
+import com.eventmanagement.services.EventService;
+import com.eventmanagement.services.ParticipantService;
+import com.eventmanagement.views.MainView;
+
+public class Main {
+    public static void main(String[] args) {
+        EventRepository eventRepository = new EventRepository();
+        ParticipantRepository participantRepository = new ParticipantRepository();
+        EventService eventService = new EventService(eventRepository, participantRepository);
+        ParticipantService participantService = new ParticipantService(participantRepository, eventRepository);
+        MainView mainView = new MainView(eventService, participantService);
+        mainView.utama();
     }
 }
--- views/EventViewImpl.java
+++ views/EventViewImpl.java
@@ -1,29 +1,19 @@
 package views;
 
-//import services.EventService;
-//import services.PesertaService;
-import entities.Event1;
-import entities.Event2;
+import services.EventService;
+
 //nunggu selesai entities sama services
 
-import java.util.List;
 import java.util.Scanner;
 
 public class EventViewImpl implements EventView{
-//    private final EventService eventService;
-//    private final PesertaService pesertaService;
-    private static Scanner scanner = new Scanner(System.in);
+    private final EventService eventService;
+    private final Scanner scanner = new Scanner(System.in);
 
-//    public EventViewImpl(EventService eventService, PesertaService pesertaService){
-//        this.eventService = eventService;
-//        this.pesertaService = pesertaService;
-//    }
-
-    public String input(String info){
-        System.out.println(info + " : ");
-        return scanner.nextLine();
-
+    public EventViewImpl(EventService eventService){
+        this.eventService = eventService;
     }
+
 
     @Override
     public void showMainMenu() {
